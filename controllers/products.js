d389866c915d48852e47cc97f3413ea7c51d0910
Abhay Rai <abhay.rai8299@gmail.com>
2022-06-02 10:20:43 +0530

    store_api

---
+++ controllers/products.js
@@ -0,0 +1,26 @@
+
+const Product=require('../models/product')
+const getAllProductsStatic=async(req,res)=>{
+    const products=await Product.find({
+     page:'2',    
+    })
+    res.status(200).json({featured:true,
+        nbHits:products.length})
+}
+
+const getAllProducts=async(req,res)=>{
+    const {featured}=req.query
+
+    const queryObject={}
+    if(featured)
+    {
+        queryObject.featured=featured==='true'? true:false
+    }console.log(queryObject)
+    const products=await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
+}
+
+module.exports={
+    getAllProducts,
+    getAllProductsStatic,
+}
